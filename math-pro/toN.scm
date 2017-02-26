6b07c81265c4c330f3b8a7eaa8a045e52f92badf
Destinyside <destinyside@163.com>
2017-02-26 21:16:25 +0800

    add new math programs

---
+++ math-pro/toN.scm
@@ -0,0 +1,17 @@
+;It can convert decimal numbers to other formats,such as hex,octal and binary 
+
+(define (to n num)
+  (define p '())
+  (define (iter i)
+    (if (< n 2)
+      (error "Can't Convert!")
+      (if (>= i n)
+	(begin 
+	  (set! p (append (list (modulo i n)) p))
+	  (iter (/ (- i (modulo i n)) n))
+	  )
+	(set! p (append (list i) p)))))
+  (iter num)
+  (display p)
+  )
+

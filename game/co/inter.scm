211f3ce11c5c030a27fad2a4d5c4957869c0a1de
Destinyside <destinyside@163.com>
2017-04-20 11:53:49 +0800

    c0's update

--- game/co/inter.scm
+++ game/co/inter.scm
@@ -1,11 +1,10 @@
-;; a interpreter of c0
-(load "stack.scm")
+;; an interpreter of c0
+(load "../../struct/stackfp.scm")
 (define (make-list num)
-  (if (number? num)
-    (cond
-      ((<= num 1) 0)
-      (else (cons 0 (make-list (- num 1)))))
-    (error "Please input a number")))
+  (define v (make-vector num))
+  (vector-fill! v 0)
+  (vector->list v))
+
 (define (list-set! list1 i value)
   (set! v list1)
   (if (list? list1) 
@@ -13,32 +12,108 @@
       (set! v (list->vector list1))
       (vector-set! v i value)
       (set! v (vector->list v))
-  ))
+      ))
   v
   )
-(define (inter)
-  (define p (stack))
-  (lambda (opr . args)
-    (define lev (car args))
-    (display lev)
+(define ins '())
+(define ptr #f)
+(define iter 0)
+
+(define stk '())
+(define top '())
+(define global '())
+
+(define (wrt lv act)
+  (display "Stack : ")
+  (display stk)
+  (newline)
+  (display "Top : ")
+  (display top)
+  (newline)
+  (newline)
+  )
+
+(define (int lv act)
+  (case lv
+    ((0)
+     (push stk (make-list act)))
+    ((1)
+     (set! global (make-list act))))
+  )
+
+(define (red lv act)
+  (push top (read))
+  )
+
+(define (lit lv act)
+  (push top act))
+
+(define (lod lv act)
+  (case lv
+    ((0)
+       (push top (list-ref (car stk) act)))
+    ((1)
+     (push top (list-ref global act)))))
+
+(define (sto lv act)
+  (case lv
+    ((0) 
+     (begin
+       (define now (pop stk))
+       (define now-top (pop top))
+       (push stk (list-set! now act now-top))))
+    ((1)
+     (begin
+       (define now-top (pop top))
+       (set! global (list-set! global act now-top))))))
+
+(define (ret lv act)
+  (pop stk)
+  (if (empty? stk)
+    (exit))
+  (set! ptr (list-ref ins iter))
+  (eval ptr)
+  )
+
+(define (add lv act)
+  (push top (+ (pop top) (pop top))))
+(define (sub lv act)
+  (push top (- (pop top) (pop top))))
+(define (mul lv act)
+  (push top (* (pop top) (pop top))))
+(define (div lv act)
+  (push top (/ (pop top) (pop top))))
+
+(define (jmp lv act)
+  (set! iter act))
+
+(define (jpc lv act)
+  (if (not (= 0 (car top)))
+    (set! iter act)))
+
+(load "../../test/fileop.scm")
+(define (load-oprs filepath)
+  (define opr (file 'r filepath))
+  (do ((op 0 (read opr)) 
+       (lv 0 (read opr)) 
+       (act 0 (read opr)))
+    ((eof-object? op) (close-input-port opr))
+    (push ins (list op lv act))))
+
+(define (exec)
+  (load-oprs "oprs")
+  (set! ins (reverse ins))
+  (pop ins)
+  (do ((i 0 (+ i 0)))
+    ((= iter (length ins)))
+    (set! ptr (list-ref ins iter))
+    (display ptr)
     (newline)
-    (define act (cadr args))
-    (display act)
-    (case opr
-      ((int) 
-       (begin 
-	 (define (push-iter arg)
-	   (if (>= arg 1)
-	     (p 'push 0))
-	   (if (>= arg 1)
-	     (push-iter (- arg 1))))
-	 (push-iter act)))
-      ((wrt) (p 'show))
-      ((sto) (set! p (list-set! p act (p 'pop))))
-      ((lit) (p 'push act))
-      ((red) (p 'push (read)))
-      ((add) (p 'push (+ (p 'pop) (p 'pop))))
-      )))
+    (eval ptr)
+    (set! iter (+ iter 1)))
+  )
+
+(exec)
 
 
-(define k (inter))
+

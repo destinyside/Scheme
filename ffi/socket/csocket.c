53894aea973ab552f610225a0f8680713cb7f95c
Destinyside <destinyside@163.com>
2018-01-14 09:45:52 +0000

    socket ffi

--- ffi/socket/csocket.c
+++ ffi/socket/csocket.c
@@ -14,6 +14,7 @@
 #include <unistd.h>  
 
 #define SERVER_PORT 5555  
+#define BUF_SIZE 200
 
 //int send(SOCKET s, const char* buf, int len, int flags);
 //int recv(SOCKET s, char* buf, int len, int flags);
@@ -29,31 +30,7 @@ int do_socket(void){
 	return socket(AF_INET,SOCK_STREAM,0);
 }
 
-int do_bind(int sock, char* addr){
-	struct sockaddr_in server_addr;
-
-	bzero(&server_addr,sizeof(server_addr));
-
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(SERVER_PORT);
-	server_addr.sin_addr.s_addr = htonl(addr);
-
-	return bind(sock,(struct sockaddr *)&server_addr,sizeof(server_addr));
-}
-	
-int do_connect(int sock, char* addr){
-	struct sockaddr_in server_addr;
-
-	bzero(&server_addr,sizeof(server_addr));
-
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(SERVER_PORT);
-	server_addr.sin_addr.s_addr = htonl(addr);
-
-	return connect(sock,(struct sockaddr *)&server_addr,sizeof(server_addr));
-}
-
-int do_accept(int sock){
+int do_bind(int sock){
 	struct sockaddr_in server_addr;
 
 	bzero(&server_addr,sizeof(server_addr));
@@ -62,5 +39,49 @@ int do_accept(int sock){
 	server_addr.sin_port = htons(SERVER_PORT);
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	return accept(sock,(struct sockaddr *)&server_addr,sizeof(server_addr));
+	return bind(sock,(struct sockaddr *)&server_addr,sizeof(server_addr));
 }
+	
+int do_connect(int sock){
+	struct sockaddr_in server_addr;
+
+	bzero(&server_addr,sizeof(server_addr));
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(SERVER_PORT);
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	return connect(sock,(struct sockaddr *)&server_addr,sizeof(server_addr));
+}
+
+int do_accept(int sock){
+	struct sockaddr_in server_addr;
+	int addr_len = sizeof(server_addr);
+
+	bzero(&server_addr,sizeof(server_addr));
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(SERVER_PORT);
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	return accept(sock,(struct sockaddr *)&server_addr,(socklen_t*)&addr_len);
+}
+
+char* do_recv(int sock){
+	char recvBuf[BUF_SIZE];
+	//printf("waiting for data ...\n");
+	while(1){
+		int dataNum = recv(sock, recvBuf, BUF_SIZE, 0);
+		if(dataNum < 0){
+			perror("recv");
+			continue;
+		}
+		recvBuf[dataNum] = '\0';
+		break;
+	}
+	char* data = recvBuf;
+	//printf("%s received.\n",data);
+	return data;
+}
+
+
--- ffi/socket/socket.scm
+++ ffi/socket/socket.scm
@@ -5,17 +5,23 @@
 (load-shared-object "./csocket.so")
 (load-shared-object "libc.so.6")
 
+(define strlen
+  (foreign-procedure "strlen" (string) int))
+
 (define perror
   (foreign-procedure "perror" (string) void))
 
+(define inet-addr
+  (foreign-procedure "inet_addr" (string) int))
+
 (define socket
-  (foreign-procedure "do_socket" (void) int))
+  (foreign-procedure "do_socket" () int))
 
 (define bind
-  (foreign-procedure "do_bind" (int string) int))
+  (foreign-procedure "do_bind" (int) int))
 
 (define connect
-  (foreign-procedure "do_connect" (int string) int))
+  (foreign-procedure "do_connect" (int) int))
 
 (define accept 
   (foreign-procedure "do_accept" (int) int))
@@ -24,13 +30,19 @@
   (foreign-procedure "listen" (int int) int))
 
 (define close
-  (foreign-procedure "close" (int int) int))
+  (foreign-procedure "close" (int) int))
 
 (define send
   (foreign-procedure "send" (int string int int) int))
 
-(define recv
-  (foreign-procedure "recv" (int string int int) int))
+(define do-send
+  (lambda (sock msg)
+    (display (string-append "send a message : " msg))
+    (newline)
+    (send sock msg (strlen msg) 0)))
+
+(define do-recv
+  (foreign-procedure "do_recv" (int) string))
 
 (define fork
   (foreign-procedure "fork" () int))
@@ -41,28 +53,68 @@
 (define check
   (lambda (who x)
     (if (< x 0)
-      (errorf who (perror (symbol->string who)))
+      (perror (string-append "err : " (symbol->string who)))
       x)))
-
-(define setup-server-socket
-  (lambda (addr)
-    (let ([sock (check 'socket (socket))])
-      (check 'bind (bind sock addr))
-      (check 'listen (listen sock 5))
-      sock))) 
-
-(define setup-client-socket
-  (lambda (addr)
-    (let ([sock (check 'socket (socket))])
-      (check 'connect (connect sock addr))
-      sock))) 
 
 (define accept-socket
   (lambda (sock)
-    (check 'accept (accept sock)))) 
+    (do ([temp (accept sock)])
+      ((> temp 0) temp))))
 
 (define pkill
   (lambda (pid)
     (define sigterm 15)
     (kill pid sigterm)
     (void)))
+
+(define setup-server-socket
+  (lambda ()
+    (let ([sock (check 'socket (socket))])
+      (check 'bind (bind sock))
+      (check 'listen (listen sock 5))
+      sock))) 
+
+(define setup-client-socket
+  (lambda ()
+    (let ([sock (check 'socket (socket))])
+      (check 'connect (connect sock))
+      sock))) 
+
+#!eof
+
+;; these two part of codes should be run in different terminal and the server runs first.
+
+;; the server
+(let ([pid (setup-server-socket)])
+  (display "Server started ...")
+  (newline)
+  (let ([client (accept-socket pid)])
+    (display "A client connected ...")
+    (newline)
+    (do ([data (do-recv client) (do-recv client)])
+      ((and (string? data) (string=? data "quit")) (begin (close client) (quit)))
+      (display data)
+      (newline)
+      (if (string=? data "")
+	(begin
+	  (close client)
+	  (quit))
+      (do-send client (string-append "the data is " data)))
+      (newline))))
+
+;; the client
+(let ([server (setup-client-socket)])
+  (display "Client started ...")
+  (newline)
+  (display "send message : ")
+  (do ([message (read) (read)])
+    ((and (string? message) (string=? message "quit")) 
+     (begin 
+       (do-send server "quit")
+       (close server) 
+       (quit)))
+    (do-send server message)
+    (let ([data (do-recv server)])
+      (display data)
+      (newline)
+      (display "send message : "))))
